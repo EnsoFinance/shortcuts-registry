8431d266a573942d1b1707ab14c20e11d86e31c6
PeterMPhillips <github@pmphillips.ca>
2024-12-11 15:16:24 -0800

    use try catch

--- scripts/simulateShortcut.ts
+++ scripts/simulateShortcut.ts
@@ -8,36 +8,39 @@ const fromAddress = '0x93621DCA56fE26Cdee86e4F6B18E116e9758Ff11';
 const weirollWalletAddress = '0xBa8F5f80C41BF5e169d9149Cd4977B1990Fc2736';
 
 async function main() {
-    const {shortcut, chainId} = await getShortcut();
+    try {
+        const {shortcut, chainId} = await getShortcut();
 
-    const args: string[] = process.argv.slice(4);
-    if (args.length != 1) throw 'Error: Please pass amounts (use commas for multiple values)';
-    const amountsIn = args[0].split(',');
-
-    const { tokensIn, tokensOut } = shortcut.inputs[chainId];
-
-    if (amountsIn.length != tokensIn.length) throw `Error: Incorrect number of amounts for shortcut. Expected ${tokensIn.length}`;
+        const args: string[] = process.argv.slice(4);
+        if (args.length != 1) throw 'Error: Please pass amounts (use commas for multiple values)';
+        const amountsIn = args[0].split(',');
     
-    const { commands, state, value } = await shortcut.build(chainId);
-    const data = weirollWalletInterface.encodeFunctionData('executeWeiroll', [commands, state]);
-    const tx: APITransaction = {
-        from: fromAddress,
-        to: weirollWalletAddress,
-        data,
-        value,
-        spender: weirollWalletAddress,
-        receiver: weirollWalletAddress,
+        const { tokensIn, tokensOut } = shortcut.inputs[chainId];
+    
+        if (amountsIn.length != tokensIn.length) throw `Error: Incorrect number of amounts for shortcut. Expected ${tokensIn.length}`;
+        
+        const { commands, state, value } = await shortcut.build(chainId);
+        const data = weirollWalletInterface.encodeFunctionData('executeWeiroll', [commands, state]);
+        const tx: APITransaction = {
+            from: fromAddress,
+            to: weirollWalletAddress,
+            data,
+            value,
+            spender: weirollWalletAddress,
+            receiver: weirollWalletAddress,
+        }
+        const request: QuoteRequest = {
+            chainId,
+            transactions: [tx],
+            tokenIn: tokensIn,
+            tokenOut: tokensOut,
+            amountIn: amountsIn,
+        }
+        const quote = await simulateTransactionOnQuoter(request);
+        console.log('Quote: ', quote[0])
+    } catch (e) {
+        console.log(e);
     }
-    const request: QuoteRequest = {
-        chainId,
-        transactions: [tx],
-        tokenIn: tokensIn,
-        tokenOut: tokensOut,
-        amountIn: amountsIn,
-
-    }
-    const quote = await simulateTransactionOnQuoter(request);
-    console.log('Quote: ', quote[0])
 }
 
 main();
