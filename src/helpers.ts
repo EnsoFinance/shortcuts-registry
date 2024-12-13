d6d755748cd9cefc1a844adfa016d79cda1553bb
victor <wiviktor@gmail.com>
2024-12-13 12:20:10 +0100

    fix(strategy): fixed amountsIn processing

--- src/helpers.ts
+++ src/helpers.ts
@@ -98,6 +98,8 @@ export function getBlockNumberFromArgs(args: string[]): number {
 
 export function getAmountsInFromArgs(args: string[]): string[] {
   const filteredArgs = args.slice(5);
+  console.log('Filtered args: ', filteredArgs);
   if (filteredArgs.length != 1) throw 'Error: Please pass amounts (use commas for multiple values)';
-  return args[0].split(',');
+
+  return filteredArgs[0].split(',');
 }
