808d87ddeb478abe453dc961c4c06e8ce3de14a6
victor <wiviktor@gmail.com>
2024-12-17 16:23:44 +0100

    feat(registry): amend weth address, checksum addresses

--- src/shortcuts/kodiak/weth-honey.ts
+++ src/shortcuts/kodiak/weth-honey.ts
@@ -4,6 +4,7 @@ import { walletAddress } from '@ensofinance/shortcuts-builder/helpers';
 import { AddressArg, ChainIds, WeirollScript } from '@ensofinance/shortcuts-builder/types';
 import { Standards, getStandardByProtocol } from '@ensofinance/shortcuts-standards';
 import { TokenAddresses } from '@ensofinance/shortcuts-standards/addresses';
+import { getAddress } from '@ethersproject/address';
 
 import { chainIdToTokenHolder } from '../../constants';
 import { AddressData, Input, Output, Shortcut } from '../../types';
@@ -15,11 +16,11 @@ export class KodiakWethHoneyShortcut implements Shortcut {
   supportedChains = [ChainIds.Cartio];
   inputs: Record<number, Input> = {
     [ChainIds.Cartio]: {
-      weth: TokenAddresses.cartio.weth,
-      usdc: TokenAddresses.cartio.usdc,
-      honey: TokenAddresses.cartio.honey,
-      island: '0xD4570a738675fB2c31e7b7b88998EE73E9E17d49',
-      primary: Standards.Kodiak_Islands.protocol.addresses!.cartio!.router,
+      weth: getAddress('0x2d93FbcE4CffC15DD385A80B3f4CC1D4E76C38b3') as AddressArg,
+      usdc: getAddress(TokenAddresses.cartio.usdc) as AddressArg,
+      honey: getAddress(TokenAddresses.cartio.honey) as AddressArg,
+      island: getAddress('0xD4570a738675fB2c31e7b7b88998EE73E9E17d49') as AddressArg,
+      primary: getAddress(Standards.Kodiak_Islands.protocol.addresses!.cartio!.router) as AddressArg,
     },
   };
 
@@ -34,8 +35,8 @@ export class KodiakWethHoneyShortcut implements Shortcut {
       tokensOut: [island],
     });
     const kodiak = getStandardByProtocol('kodiak-islands', chainId);
-    const usdcAmount = await builder.add(balanceOf(usdc, walletAddress()));
-    const wethAmount = await builder.add(balanceOf(weth, walletAddress()));
+    const usdcAmount = builder.add(balanceOf(usdc, walletAddress()));
+    const wethAmount = builder.add(balanceOf(weth, walletAddress()));
     const mintedAmount = await mintHoney(usdc, usdcAmount, builder);
 
     await kodiak.deposit.addToBuilder(builder, {
@@ -45,7 +46,7 @@ export class KodiakWethHoneyShortcut implements Shortcut {
       primaryAddress: primary,
     });
 
-    const honeyAmount = await builder.add(balanceOf(honey, walletAddress()));
+    const honeyAmount = builder.add(balanceOf(honey, walletAddress()));
     await redeemHoney(honey, honeyAmount, builder);
 
     const payload = await builder.build({
