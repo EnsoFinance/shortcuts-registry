1aeb0c5f90f78972f0e4aad688fe9be782110ec5
victor <wiviktor@gmail.com>
2024-12-17 13:21:10 +0100

    feature(strategy): adatpted dolomit wbtc

--- src/shortcuts/dolomite/dwbtc.ts
+++ src/shortcuts/dolomite/dwbtc.ts
@@ -1,11 +1,13 @@
 import { Builder } from '@ensofinance/shortcuts-builder';
 import { RoycoClient } from '@ensofinance/shortcuts-builder/client/implementations/roycoClient';
 import { walletAddress } from '@ensofinance/shortcuts-builder/helpers';
-import { ChainIds, WeirollScript } from '@ensofinance/shortcuts-builder/types';
+import { AddressArg, ChainIds, WeirollScript } from '@ensofinance/shortcuts-builder/types';
 import { getStandardByProtocol } from '@ensofinance/shortcuts-standards';
 import { TokenAddresses } from '@ensofinance/shortcuts-standards/addresses';
+import { getAddress } from '@ethersproject/address';
 
-import { Input, Output, Shortcut } from '../../types';
+import { chainIdToTokenHolder } from '../../constants';
+import type { AddressData, Input, Output, Shortcut } from '../../types';
 import { balanceOf } from '../../utils';
 
 export class DolomiteDWbtcShortcut implements Shortcut {
@@ -14,8 +16,8 @@ export class DolomiteDWbtcShortcut implements Shortcut {
   supportedChains = [ChainIds.Cartio];
   inputs: Record<number, Input> = {
     [ChainIds.Cartio]: {
-      base: TokenAddresses.cartio.wbtc,
-      vault: '0x29cF6e8eCeFb8d3c9dd2b727C1b7d1df1a754F6f', //dwbtc
+      base: getAddress(TokenAddresses.cartio.wbtc) as AddressArg,
+      vault: getAddress('0x29cF6e8eCeFb8d3c9dd2b727C1b7d1df1a754F6f') as AddressArg, //dwbtc
     },
   };
 
@@ -51,5 +53,24 @@ export class DolomiteDWbtcShortcut implements Shortcut {
       script: payload.shortcut as WeirollScript,
       metadata: builder.metadata,
     };
+  }
+
+  getAddressData(chainId: number): Map<AddressArg, AddressData> {
+    switch (chainId) {
+      case ChainIds.Cartio:
+        return new Map([
+          [this.inputs[ChainIds.Cartio].base, { label: 'ERC20:WBTC' }],
+          [this.inputs[ChainIds.Cartio].vault, { label: 'ERC20:dWBTC' }],
+        ]);
+      default:
+        throw new Error(`Unsupported chainId: ${chainId}`);
+    }
+  }
+
+  getTokenHolder(chainId: number): Map<AddressArg, AddressArg> {
+    const tokenToHolder = chainIdToTokenHolder.get(chainId);
+    if (!tokenToHolder) throw new Error(`Unsupported 'chainId': ${chainId}`);
+
+    return tokenToHolder as Map<AddressArg, AddressArg>;
   }
 }
