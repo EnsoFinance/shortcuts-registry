cd1a5d4b2b7187c8c4b5b301255163a239233589
PeterMPhillips <github@pmphillips.ca>
2024-12-19 12:55:07 -0800

    support setter and multicall simulations

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,5 @@
 import { Builder } from '@ensofinance/shortcuts-builder';
-import { contractCall } from '@ensofinance/shortcuts-builder/helpers';
+import { contractCall, walletAddress } from '@ensofinance/shortcuts-builder/helpers';
 import {
   AddressArg,
   ChainIds,
@@ -8,10 +8,21 @@ import {
   Transaction,
   WalletAddressArg,
 } from '@ensofinance/shortcuts-builder/types';
-import { Standards, getStandardByProtocol } from '@ensofinance/shortcuts-standards';
+import { PUBLIC_RPC_URLS, Standards, getStandardByProtocol } from '@ensofinance/shortcuts-standards';
 import { TokenAddresses } from '@ensofinance/shortcuts-standards/addresses';
+import { addAction, areAddressesEqual, resetApprovals } from '@ensofinance/shortcuts-standards/helpers';
+import { Interface } from '@ethersproject/abi';
+import { StaticJsonRpcProvider } from '@ethersproject/providers';
 
 import type { RoycoOutput, Shortcut, SimulationResult } from '../types';
+
+export const addresses: Record<number, Record<string, AddressArg>> = {
+  [ChainIds.Cartio]: {
+    setter: '0x67D0B6e109b82B51706dC4D71B42Bf19CdFC8d1e',
+    honey: TokenAddresses.cartio.honey,
+    honeyFactory: Standards.Berachain_Honey.protocol.addresses!.cartio!.honeyFactory,
+  },
+};
 
 export async function prepareResponse(
   /* eslint-disable @typescript-eslint/no-explicit-any */
@@ -35,12 +46,12 @@ export function balanceOf(token: AddressArg, owner: WalletAddressArg) {
 }
 
 export async function mintHoney(asset: AddressArg, amount: NumberArg, builder: Builder) {
-  const honey = getStandardByProtocol('berachain-honey', builder.chainId);
-  const honeyFactory = Standards.Berachain_Honey.protocol.addresses!.cartio!.honeyFactory;
+  const berachainHoney = getStandardByProtocol('berachain-honey', builder.chainId);
+  const { honey, honeyFactory } = addresses[builder.chainId];
 
-  const { amountOut } = await honey.deposit.addToBuilder(builder, {
+  const { amountOut } = await berachainHoney.deposit.addToBuilder(builder, {
     tokenIn: asset,
-    tokenOut: TokenAddresses.cartio.honey,
+    tokenOut: honey,
     amountIn: amount,
     primaryAddress: honeyFactory,
   });
@@ -49,17 +60,65 @@ export async function mintHoney(asset: AddressArg, amount: NumberArg, builder: B
 }
 
 export async function redeemHoney(asset: AddressArg, amount: NumberArg, builder: Builder) {
-  const honey = getStandardByProtocol('berachain-honey', builder.chainId);
-  const honeyFactory = Standards.Berachain_Honey.protocol.addresses!.cartio!.honeyFactory;
+  const berachainHoney = getStandardByProtocol('berachain-honey', builder.chainId);
+  const { honey, honeyFactory } = addresses[builder.chainId];
 
-  const { amountOut } = await honey.redeem.addToBuilder(builder, {
-    tokenIn: TokenAddresses.cartio.honey,
+  const { amountOut } = await berachainHoney.redeem.addToBuilder(builder, {
+    tokenIn: honey,
     tokenOut: asset,
     amountIn: amount,
     primaryAddress: honeyFactory,
   });
 
   return amountOut as FromContractCallArg;
+}
+
+export async function depositKodiak(
+  builder: Builder,
+  tokensIn: AddressArg[],
+  amountsIn: NumberArg[],
+  island: AddressArg,
+  primary: AddressArg,
+  setter: AddressArg,
+) {
+  const rpcUrl = PUBLIC_RPC_URLS[builder.chainId].rpcUrls.public;
+  const provider = new StaticJsonRpcProvider(rpcUrl);
+  const islandInterface = new Interface(['function token0() external view returns(address)']);
+  const token0Bytes = await provider.call({
+    to: island,
+    data: islandInterface.encodeFunctionData('token0', []),
+  });
+  const token0 = '0x' + token0Bytes.slice(26);
+  const [amount0, amount1] = areAddressesEqual(token0, tokensIn[0])
+    ? [amountsIn[0], amountsIn[1]]
+    : [amountsIn[1], amountsIn[0]];
+  const approvals = {
+    tokens: tokensIn,
+    amounts: amountsIn,
+    spender: primary,
+  };
+  const amountSharesMin = builder.add({
+    address: setter,
+    abi: ['function getSingleValue() external view returns (uint256)'],
+    functionName: 'getSingleValue',
+    args: [],
+  });
+  await addAction({
+    builder,
+    action: {
+      address: primary,
+      abi: [
+        'function addLiquidity(address island, uint256 amount0Max, uint256 amount1Max, uint256 amount0Min, uint256 amount1Min, uint256 amountSharesMin, address receiver) returns (uint256 amount0, uint256 amount1, uint256 mintAmount)',
+      ],
+      functionName: 'addLiquidity',
+      args: [island, amount0, amount1, 0, 0, amountSharesMin, walletAddress()],
+    },
+    approvals,
+  });
+  await resetApprovals(builder, {
+    tokens: tokensIn,
+    spender: primary,
+  });
 }
 
 export async function buildRoycoMarketShortcut(shortcut: Shortcut, chainId: ChainIds): Promise<RoycoOutput> {
