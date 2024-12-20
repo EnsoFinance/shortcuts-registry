569dd5801f46084ad8b4bc790408e1aa97aa01b3
victor <wiviktor@gmail.com>
2024-12-20 19:47:55 +0100

    fix(registry): fixed bad addresses after refactor

--- src/shortcuts/beraborrow/mint-nect-lp.ts
+++ src/shortcuts/beraborrow/mint-nect-lp.ts
@@ -6,7 +6,7 @@ import { Standards, getStandardByProtocol } from '@ensofinance/shortcuts-standar
 import { div } from '@ensofinance/shortcuts-standards/helpers/math';
 import { getAddress } from '@ethersproject/address';
 
-import { chainIdToDeFiAddresses, chainIdToTokenHolder } from '../../constants';
+import { chainIdToDeFiAddresses, chainIdToSimulationRoles, chainIdToTokenHolder } from '../../constants';
 import type { AddressData, Input, Output, Shortcut } from '../../types';
 import { balanceOf, depositKodiak, mintHoney } from '../../utils';
 
@@ -22,8 +22,8 @@ export class BeraborrowMintNectLpShortcut implements Shortcut {
       usdcPsmBond: getAddress('0xd064C80776497821313b1Dc0E3192d1a67b2a9fa') as AddressArg,
       island: getAddress(Standards.Kodiak_Islands.protocol.addresses!.cartio!.nectUsdcIsland) as AddressArg, // KODI-HONEY-NECT
       primary: chainIdToDeFiAddresses[ChainIds.Cartio].kodiakRouter,
-      quoterV2: chainIdToDeFiAddresses[ChainIds.Cartio].quoterV2,
-      setter: chainIdToDeFiAddresses[ChainIds.Cartio].setter, // having setter in inputs lets simulator know to set a min amount value
+      quoterV2: chainIdToDeFiAddresses[ChainIds.Cartio].kodiakQuoterV2,
+      setter: chainIdToSimulationRoles.get(ChainIds.Cartio)!.setter.address!, // having setter in inputs lets simulator know to set a min amount value
     },
   };
 
